370aab76bb85a594f798408766aac314ea856cb6
whoxer <natanaelvsant@gmail.com>
2024-12-25 02:12:01 -0300

    Elemento h1 removido de App

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Card from './Card';
 function App() {
   return (
     <div className="App">
-      <h1>Abra a carta!</h1>
       <Card />
-
       <h4 className="description">feito com amor e carinho por nat 💜</h4>
     </div>
   );
