d286119edc0ec4c69ffb5195ca3936f1fbb9cd2d
whoxer <natanaelvsant@gmail.com>
2024-12-25 02:31:44 -0300

    Texto de amor

--- src/LoveText.js
+++ src/LoveText.js
@@ -1,13 +1,47 @@
+import "./LoveText.css"
 import React from 'react';
 
 function LoveText() {
     return (
-        <div>
-            <h2>Titulo</h2>
-            <p>
-                texto
-            </p>
-        </div>
+      <div>
+        <h1 className="title">Feliz natal!</h1>
+        <br></br>
+        <p className="text">
+          É com muita alegria que eu te escrevo essas palavras, você sabe que eu
+          poderia simplesmente te mandar mensagem ou mandar um áudio dizendo
+          todas essas coisas, mas aí não seria eu, seria outra pessoa. E eu
+          confesso que eu seria uma pessoa muito menos feliz se fosse qualquer
+          outra pessoa no meu lugar.
+        </p>
+        <br></br>
+        <p className="text">
+          Dio, meu amor, encontrar você nesse universo tão grande realmente foi
+          um gigantesco achado, acho que a ciência jamais fará tamanha
+          descoberta quanto quando eu descobri que eu podia amar uma pessoa da
+          forma como eu te amo novamente.
+        </p>
+        <br></br>
+        <p className="text">
+          Eu dedico essa mensagem não somente a ti mas a todo o apoio que você
+          tem me dado desde que voltamos a nos falar, e a todo esforço que você
+          coloca e colocou não somente por mim mas por outras pessoas na luta
+          popular pelos direitos das pessoas trans, é preciso muito ódio mas
+          também muito amor para fazer o que fazemos e eu acredito que amar é
+          uma maneira de resistir.
+        </p>
+        <br></br>
+        <p className="text">
+          Muito obrigado por todos os sorrisos que você pôs em meu rosto,
+          agradeço também por toda a alegria que você me proporcionou durante
+          esse tempo em que estamos tão longe um do outro, eu sou muito grato
+          pela sua existência e pelo seu amor. Muito obrigado mesmo.
+        </p>
+        <br></br>
+        <p className="text">
+          Feliz natal para você e para toda a sua família, que dias melhores
+          venham para nós e toda a humanidade!
+        </p>
+      </div>
     );
 }
 
